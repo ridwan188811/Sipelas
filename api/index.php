5412c5fce5e39e8be18104f302b86656aae913c0
unknown <mamalemon193@gmail.com>
2026-06-24 21:45:37 +0700

    Fix Vercel 500 error by overriding storage variables in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,13 @@
 <?php
 
+$_ENV['APP_STORAGE'] = '/tmp';
+putenv('APP_STORAGE=/tmp');
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+
 require __DIR__ . '/../public/index.php';
+
