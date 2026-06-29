1d380852886fa5dd5fdafeb7668c026fb2447f70
unknown <mamalemon193@gmail.com>
2026-06-30 01:04:31 +0700

    Fix: Remove hardcoded maintenance mode from api/index.php

--- api/index.php
+++ api/index.php
@@ -1,8 +1,5 @@
 <?php
 
-http_response_code(503);
-echo "<h1>503 Service Unavailable</h1><p>Website Kelurahan saat ini sedang dalam pemeliharaan sistem (Maintenance Server dan Database). Silakan kembali beberapa saat lagi.</p>";
-exit;
 
 $_ENV['APP_STORAGE'] = '/tmp';
 putenv('APP_STORAGE=/tmp');
