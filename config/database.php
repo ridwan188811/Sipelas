966da0e9635485f1d12e0365c732b4f593de207b
unknown <mamalemon193@gmail.com>
2026-07-06 02:02:00 +0700

    fix(db): ensure correct vercel connection parsing

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_URL') ? (str_starts_with(env('DB_URL'), 'postgres') ? 'pgsql' : env('DB_CONNECTION', 'mysql')) : env('DB_CONNECTION', 'mysql'),
+    'default' => (env('DATABASE_URL') && str_starts_with(env('DATABASE_URL'), 'postgres')) || (env('DB_URL') && str_starts_with(env('DB_URL'), 'postgres')) ? 'pgsql' : env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
